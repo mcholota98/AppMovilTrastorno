e833e56c8b7b6a0cb79226eb881da782fb6c4b70
mcholota98 <miguelcholota1@gmail.com>
2022-08-03 06:14:09 -0500

    Segundo

---
+++ app/src/main/java/com/example/appmoviltrastorno/people_to_monitor.java
@@ -0,0 +1,14 @@
+package com.example.appmoviltrastorno;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class people_to_monitor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_people_to_monitor);
+    }
+}
---
+++ app/src/main/java/com/example/appmoviltrastorno/see_detect.java
@@ -0,0 +1,14 @@
+package com.example.appmoviltrastorno;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class see_detect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_see_detect);
+    }
+}
