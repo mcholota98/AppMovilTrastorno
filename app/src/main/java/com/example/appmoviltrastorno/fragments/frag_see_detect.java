79596fa23008f79ef634c5a99d5fd54cbeb4f7fe
mcholota98 <miguelcholota1@gmail.com>
2022-08-03 21:20:59 -0500

    Tercero

---
+++ app/src/main/java/com/example/appmoviltrastorno/fragments/frag_see_detect.java
@@ -0,0 +1,73 @@
+package com.example.appmoviltrastorno.fragments;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Switch;
+
+import com.example.appmoviltrastorno.R;
+import androidx.fragment.app.Fragment;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class frag_see_detect extends Fragment  {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    private String link_api;
+    private ProgressDialog progDailog;
+    private Switch swSistema;
+    private JSONObject json_data;
+
+    public frag_see_detect() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Ajustes.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static frag_see_detect newInstance(String param1, String param2) {
+        frag_see_detect fragment = new frag_see_detect();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_see_detect, container, false);
+        return rootView;
+    }
+
+
+
+
+
+}
--- app/src/main/java/com/example/appmoviltrastorno/menu.java
+++ app/src/main/java/com/example/appmoviltrastorno/menu.java
@@ -1,14 +1,42 @@
 package com.example.appmoviltrastorno;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
-public class menu extends AppCompatActivity {
+import com.example.appmoviltrastorno.adapters.PagerController;
+import com.google.android.material.tabs.TabLayout;
 
+public class menu extends AppCompatActivity implements TabLayout.OnTabSelectedListener{
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    PagerController pageAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        tabLayout = findViewById(R.id.tbMenuPrincipal);
+        viewPager = findViewById(R.id.viewpager);
+        pageAdapter = new PagerController(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(pageAdapter);
+        tabLayout.setOnTabSelectedListener(this);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+    }
+
+    @Override
+    public void onTabSelected(TabLayout.Tab tab) {
+        viewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(TabLayout.Tab tab) {
+
+    }
+
+    @Override
+    public void onTabReselected(TabLayout.Tab tab) {
+
     }
 }
